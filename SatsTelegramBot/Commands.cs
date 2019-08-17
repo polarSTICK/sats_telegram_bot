a02b460e2557ce8fe94103bdf4da2cbe2c7798f7
Mikael Mäkelä <makelamikael1@gmail.com>
2019-08-18 01:03:10 +0300

    Project initialization

---
+++ SatsTelegramBot/Commands.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using System.Text.RegularExpressions;
+
+namespace SatsTelegramBot
+{
+    public class Commands
+    {
+        private readonly TelegramBotClient _bot;
+        private readonly MessageEventArgs _messageEventArgs;
+        private readonly Random _rnd = new Random();
+
+        public Commands(TelegramBotClient bot, MessageEventArgs messageEventArgs)
+        {
+            _bot = bot;
+            _messageEventArgs = messageEventArgs;
+        }
+
+        public void Start()
+        {
+            _bot.SendTextMessageAsync(_messageEventArgs.Message.Chat.Id, @"Usage:
+/roll - To roll the dice. Specify a number of the dice and how many sides the dice needs to have. Like this '4d6'
+");
+        }
+
+        public void RollDiceNew() //Todo: hit counting, dice roller
+        {
+            if (_messageEventArgs.Message.Text.Contains("d"))
+            {
+                var message = _messageEventArgs.Message.Text.Replace("/roll ", "");
+
+                int total = 0;
+
+                var rawDice = message.Split('d');
+
+                int numberOfDice = int.Parse(rawDice[0]);
+                int numberOfSides = int.Parse(rawDice[1]);
+
+                string[] allRolledDice = new string[numberOfDice];
+
+                for (int i = 0; i < numberOfDice; i++)
+                {
+                    var rolledDice = _rnd.Next(1, numberOfSides);
+                    total += rolledDice;
+                    allRolledDice[i] = rolledDice.ToString();
+                }
+
+                string result = string.Join(", ", allRolledDice.OrderByDescending(x => x));
+
+                _bot.SendTextMessageAsync(_messageEventArgs.Message.Chat.Id, $"Rolled dice: {result} \nwith total: {total}");
+            }
+            else
+            {
+                _bot.SendTextMessageAsync(_messageEventArgs.Message.Chat.Id, "specify the amount of dice and kind of dice.");
+                _bot.SendTextMessageAsync(_messageEventArgs.Message.Chat.Id, "dipshit.");
+            }
+        }
+
+        public void RollDice()
+        {
+            Regex rx = new Regex(@"\/\b\broll\b(\s\d+[d]\d+)+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            if (!rx.IsMatch(_messageEventArgs.Message.Text))
+            {
+                Error();
+                _bot.SendTextMessageAsync(_messageEventArgs.Message.Chat.Id, "dipshit.");
+                return;
+            }
+
+            var stringArray = _messageEventArgs.Message.Text.Split(' ');
+
+            for (int i = 1; i < stringArray.Length; i++)
+            {
+                var rawDice = stringArray[i].Split('d');
+
+                var total = 0;
+
+                int diceAmount = int.Parse(rawDice[0]);
+                int sides = int.Parse(rawDice[1]);
+
+                int[] allRolledDice = new int[diceAmount];
+
+                for (int j = 0; j < diceAmount; j++)
+                {
+                    var rolledDice = _rnd.Next(1, sides);
+                    total += rolledDice;
+                    allRolledDice[j] = rolledDice;
+                }
+
+                var result = string.Join(", ", allRolledDice.OrderByDescending(x => x));
+
+                _bot.SendTextMessageAsync(_messageEventArgs.Message.Chat.Id, $"Rolled d{sides}: {result} \nwith total: {total}");
+            }
+        }
+
+        public void CallMeDaddy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsultReplied()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowWeather()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SummonPerson()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateFeedback()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(Exception e = null)
+        {
+            if (e != null)
+            {
+                _bot.SendTextMessageAsync(_messageEventArgs.Message.Chat.Id, $"Would you look at that, you made me crash. Good job, you twat. Enjoy the error message: {e.Message}");
+                return;
+            }
+
+            _bot.SendTextMessageAsync(_messageEventArgs.Message.Chat.Id, "Input was incorrect");
+        }
+    }
+}
---
+++ SatsTelegramBot/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace SatsTelegramBot
+{
+    public class Program
+    {
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("805960903:AAGnFRDO08e0R7mGL_UcyMy0Kz9UJZuHDEw");
+        
+        static void Main(string[] args)
+        {
+            Bot.OnMessage += Bot_OnMessage;
+
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            var cmd = new Commands(Bot, e);
+
+            Console.WriteLine(e.Message.From.Id.ToString());
+            Console.WriteLine(e.Message.From.Username);
+
+            try
+            {
+                if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+                {
+                    if (e.Message.Text.ToCharArray()[0] != '/') return;
+
+                    var wordArray = e.Message.Text.Split(' ');
+                    var command = wordArray[0].Replace("@sats_telegram_bot", "");
+
+                    switch (command)
+                    {
+                        case "/start":
+                            //cmd.Start();
+                            break;
+                        case "/roll":
+                            cmd.RollDice();
+                            break;
+                        case "/daddy":
+                            cmd.CallMeDaddy();
+                            break;
+                        case "/insult":
+                            cmd.InsultReplied();
+                            break;
+                        case "/weather":
+                            cmd.ShowWeather();
+                            break;
+                        case "/summon":
+                            cmd.SummonPerson();
+                            break;
+                        case "/feedback":
+                            cmd.CreateFeedback();
+                            break;
+                        default:
+                            Bot.SendTextMessageAsync(e.Message.Chat.Id, "Your query is unrecognized, make it better, dipshit.");
+                            break;
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                cmd.Error(exception);
+            }
+        }
+    }
+}
